AOKP/device_hp_tenderloin

more camera fixes

BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := scorpion
 TARGET_CPU_SMP := true
 
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=59 -DQCOM_HARDWARE -DNEEDS_VECTORIMPL_SYMBOLS
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=59 -DQCOM_HARDWARE -DNEEDS_VECTORIMPL_SYMBOLS -DICS_CAMERA_BLOB
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 768

invisiblek/android_device_hp_tenderloin

Conflicts:
	BoardConfig.mk
Change-Id: I06387542faa0649a0d58dc1c40144c01e54a5b6c

BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_RECOVERY_FSTAB := device/hp/tenderloin/fstab.tenderloin
 RECOVERY_FSTAB_VERSION := 2
 
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=59 -DQCOM_HARDWARE -DQCOM_NO_SECURE_PLAYBACK
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=59 -DQCOM_HARDWARE -DQCOM_NO_SECURE_PLAYBACK -DICS_CAMERA_BLOB
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER      	:= NL80211