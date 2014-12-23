b7e8c0c7214385a24b300ec824d1cd5ffede28d1
Dan Pasanen <dan.pasanen@gmail.com>
2014-12-22 23:49:21 -0600

    [WIP] sepolicy: fix a few denials
    * rebootcmd is going to be a pita...
    
    Change-Id: I9e34b2c78416464fa80e2c2000d9f5b5aabec645

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,7 +157,9 @@ BOARD_SEPOLICY_UNION += \
     bootanim.te \
     file.te \
     file_contexts \
+    init_shell.te \
     lvm.te \
+    mpdecision.te \
     serial.te \
     surfaceflinger.te \
     sysinit.te
