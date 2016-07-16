1ba3951acadaa3db243b36aa91adf66293965e6c
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-07-16 17:24:30 -0500

    tenderloin: ev->cm
    * all aboard the kang train!
    
    Change-Id: Icd50964c6f3a039a80b93f25d371711b4a4b3fea

--- cm.mk
+++ cm.mk
@@ -1,13 +1,13 @@
 # Inherit AOSP device configuration for inc.
 $(call inherit-product, device/hp/tenderloin/full_tenderloin.mk)
 
-# Inherit some common evervolv stuff.
-$(call inherit-product, vendor/ev/config/common_full_tablet_wifionly.mk)
+# Inherit some common cm stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := ev_tenderloin
+PRODUCT_NAME := cm_tenderloin
 PRODUCT_BRAND := HP
 PRODUCT_DEVICE := tenderloin
 PRODUCT_MODEL := Touchpad
@@ -18,12 +18,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="hp/hp_tenderloin/tenderloin:6.0/MRA58K/2256973:user/release-keys" \
     PRIVATE_BUILD_DESC="tenderloin-user 6.0 MRA58K 2256973 release-keys" \
     BUILD_NUMBER=228551
-
-# Set up the product codename, build version & MOTD.
-PRODUCT_CODENAME := Queo
-PRODUCT_VERSION_DEVICE_SPECIFIC := p1
-
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your HP Touchpad\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
 # Bootanimation
 TARGET_BOOTANIMATION_HALF_RES := true
